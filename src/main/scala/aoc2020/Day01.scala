efbf59412b81d8eed6fe7775705e48c88706680f
George Samidare <larzone@gmail.com>
2020-12-01 06:14:05 +0100

    y2020d01

--- src/main/scala/aoc2020/Day01.scala
+++ src/main/scala/aoc2020/Day01.scala
@@ -4,8 +4,19 @@ import scala.io.Source
 
 object Day01 extends App {
 
-  val input = Source.fromFile("inputs/2020/01.txt").getLines().toList
+  val input = Source.fromFile("inputs/2020/01.txt").getLines().map(_.toInt).toList
 
-  println(input)
+  val remainders = input.map(2020 - _).zip(input).toMap
 
+  println((for {
+    a <- input if remainders.contains(a)
+    b = remainders(a)
+  } yield a * b).head)
+
+  println((for {
+    i <- input.indices
+    j <- i + 1 until input.length
+    a = input(i) + input(j) if remainders.contains(a)
+    b = remainders(a)
+  } yield input(i) * input(j) * b).head)
 }
