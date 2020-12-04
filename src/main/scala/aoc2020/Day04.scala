06360e81f9526042f502a704cdd88cdc0b0d3140
George Samidare <larzone@gmail.com>
2020-12-04 06:44:38 +0100

    y2020d04

---
+++ src/main/scala/aoc2020/Day04.scala
@@ -0,0 +1,61 @@
+package aoc2020
+
+import scala.io.Source
+
+object Day04 extends App {
+
+  val passports = Source.fromFile("inputs/2020/04.txt").getLines()
+    .toList
+    .mkString("\n")
+    .split("\n\n")
+    .toList
+    .map(_.split(List(' ', '\n', ',').toArray).map(_.trim).toList)
+
+  val yr = """(\d{4})""".r
+  val hgt = """(\d+)(in|cm)""".r
+  val hcl = """#[0-9a-f]{6}""".r
+  val pid = """\d{9}""".r
+
+  val expectedFields = Map[String, String => Boolean](
+    "byr" -> {
+      case yr(y) if (1920 to 2002) contains y.toInt => true
+      case _ => false
+    },
+    "iyr" -> {
+      case yr(y) if (2010 to 2020) contains y.toInt => true
+      case _ => false
+    },
+    "eyr" -> {
+      case yr(y) if (2020 to 2030) contains y.toInt => true
+      case _ => false
+    },
+    "hgt" -> {
+      case hgt(h, cm) if cm == "cm" => (150 to 193) contains h.toInt
+      case hgt(h, in) if in == "in" => (59 to 76) contains h.toInt
+      case _ => false
+    },
+    "hcl" -> {
+      case hcl() => true
+      case _ => false
+    },
+    "ecl" -> { s => Set("amb", "blu", "brn", "gry", "grn", "hzl", "oth") contains s },
+    "pid" -> {
+      case pid() => true
+      case _ => false
+    },
+  )
+
+  def isValid(fields: List[String])(validField: List[String] => Boolean): Boolean = {
+    fields.map(_.split(':').toList).count(validField) == expectedFields.size
+  }
+
+  println(passports.count(p => isValid(p) {
+    case field :: _ :: Nil if expectedFields.contains(field) => true
+    case _ => false
+  }))
+
+  println(passports.count(p => isValid(p) {
+    case field :: value :: Nil if expectedFields.contains(field) && expectedFields(field)(value) => true
+    case _ => false
+  }))
+}
