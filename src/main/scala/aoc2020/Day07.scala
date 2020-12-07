2d03582d14de7726e4d3a72b9e439e38a97cbc3c
George Samidare <larzone@gmail.com>
2020-12-07 06:31:22 +0100

    y2020d07

---
+++ src/main/scala/aoc2020/Day07.scala
@@ -0,0 +1,38 @@
+package aoc2020
+
+import scala.io.Source
+
+object Day07 extends App {
+
+  val myBag = "shiny gold"
+
+  val input = {
+
+    def parseList(str: String) : List[(Int, String)] = {
+      val re = """(\d+) (.*) bags?""".r
+      if (str == "no other bags") Nil
+      else str.split(", ").map { case re(num, col) => (num.toInt, col)}.toList
+    }
+
+    val re = """(.*) bags contain (.*).""".r
+    val lines = Source.fromFile("inputs/2020/07.txt").getLines()
+      .map { case re(outerColor, innerList) => (outerColor, parseList(innerList))}
+      .toMap
+
+    lines
+  }
+
+  def distinctOuterColors(inner: String) : Set[String] = {
+    val canContain = input.filter(_._2.exists(_._2 == inner)).keys.toList
+    canContain.toSet ++ canContain.flatMap(distinctOuterColors)
+  }
+
+  println(distinctOuterColors(myBag).size)
+
+  def countContainingBags(outer: String): Int = {
+    val contents = input(outer)
+    contents.map(_._1).sum +  contents.map(bag => bag._1 * countContainingBags(bag._2)).sum
+  }
+
+  println(countContainingBags(myBag))
+}
