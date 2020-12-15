57874afc39ddfb1805c480ce175301a3463e4c6f
George Samidare <larzone@gmail.com>
2020-12-15 06:27:57 +0100

    y2020d15.cleanup

--- src/main/scala/aoc2020/Day15.scala
+++ src/main/scala/aoc2020/Day15.scala
@@ -12,9 +12,9 @@ object Day15 extends App {
       else if (!mem0.contains(prev)) 0
       else turn - mem0(prev) - 1
       (next, mem1, mem1.updated(next, turn), turn + 1)
-  }.map(_._1).zipWithIndex.filter {
-    case (_, 2020) => true
-    case (_, 30_000_000) => true
+  }.drop(1).filter {
+    case (_, _, _, 2020) => true
+    case (_, _, _, 30_000_000) => true
     case _ => false
   }.map(_._1).take(2).foreach(println)
 }
