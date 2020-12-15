999dce2e8cfdab312734d9a946f7baf350bc34fa
George Samidare <larzone@gmail.com>
2020-12-15 21:46:23 +0100

    y2020d15.cleanup

--- src/main/scala/aoc2020/Day15.scala
+++ src/main/scala/aoc2020/Day15.scala
@@ -6,15 +6,15 @@ object Day15 extends App {
 
   val input = Source.fromFile("inputs/2020/15.txt").getLines().next().split(",").map(_.toInt).toList
 
-  LazyList.iterate((-1, Map[Int, Int](), Map[Int, Int](), 0)) {
-    case (prev, mem0, mem1, turn) =>
+  LazyList.iterate((-1, 0, Map[Int, Int]())) {
+    case (prev, turn, mem) =>
       val next = if (turn < input.length) input(turn)
-      else if (!mem0.contains(prev)) 0
-      else turn - mem0(prev) - 1
-      (next, mem1, mem1.updated(next, turn), turn + 1)
-  }.drop(1).filter {
-    case (_, _, _, 2020) => true
-    case (_, _, _, 30_000_000) => true
+      else if (!mem.contains(prev)) 0
+      else turn - mem(prev)
+      (next, turn + 1, mem.updated(prev, turn))
+  }.map(_._1).zipWithIndex.filter {
+    case (_, 2020) => true
+    case (_, 30_000_000) => true
     case _ => false
   }.map(_._1).take(2).foreach(println)
 }
