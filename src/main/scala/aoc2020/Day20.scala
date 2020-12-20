64d77ebee99d7846398c898b57e824abec21109b
George Samidare <larzone@gmail.com>
2020-12-20 19:03:44 +0100

    y2020d20.cleanup

--- src/main/scala/aoc2020/Day20.scala
+++ src/main/scala/aoc2020/Day20.scala
@@ -4,14 +4,14 @@ import scala.io.Source
 
 object Day20 extends App {
 
-  case class Tile(id: Long, pixels: Map[(Int, Int), Boolean]) {
+  case class Tile(id: Long, pixels: Set[(Int, Int)]) {
 
-    val maxX: Int = pixels.keys.map(_._1).max
-    val maxY: Int = pixels.keys.map(_._2).max
+    val maxX: Int = pixels.map(_._1).max
+    val maxY: Int = pixels.map(_._2).max
 
     def rotate(): Tile = {
       copy(pixels = pixels.map {
-        case ((x, y), v) => (maxY - y, x) -> v
+        case (x, y) => (maxY - y, x)
       })
     }
 
@@ -21,23 +21,33 @@ object Day20 extends App {
     }
 
     def flipH: Tile = copy(pixels = pixels.map {
-      case ((x, y), v) => (maxX - x, y) -> v
+      case (x, y) => (maxX - x, y)
     })
 
     def flipV: Tile = copy(pixels = pixels.map {
-      case ((x, y), v) => (x, maxY - y) -> v
+      case (x, y) => (x, maxY - y)
     })
 
-    def viz(): Unit = {
-      val maxIdx = pixels.keys.map(_._1).max
-      for {y <- 0 to maxY} {
-        for {x <- 0 to maxX} {
-          if (pixels(x, y)) print('#')
-          else print('.')
-        }
-        println()
-      }
-    }
+    lazy val variations: Set[Tile] = for {
+      flipped <- Set(this, flipH, flipV)
+      rotations <- 0 until 4
+      rotated = flipped.rotateN(rotations)
+    } yield rotated
+  }
+
+  object Tile {
+    def apply(id: Long, strings: List[String]): Tile = new Tile(id,
+      for {
+        (line, y) <- strings.zipWithIndex.toSet
+        (ch, x) <- line.zipWithIndex if ch == '#'
+      } yield (x, y)
+    )
+
+    val seaMonster: Tile = Tile(666, List(
+      "                  # ",
+      "#    ##    ##    ###",
+      " #  #  #  #  #  #   ",
+    ))
   }
 
   val input = {
@@ -48,131 +58,91 @@ object Day20 extends App {
       val id = tile.head match {
         case re(id) => id.toInt
       }
-      val pixels = tile.tail.zipWithIndex.flatMap { case (row, y) =>
-        row.zipWithIndex.map {
-          case ('#', x) => (x, y) -> true
-          case ('.', x) => (x, y) -> false
-        }
-      }.toMap
-      Tile(id, pixels)
+      Tile(id, tile.tail)
     }
 
     lines.map(line => parseTile(line.split("\n").toList)).toSet
   }
 
-  val maxIdx = input.head.pixels.keys.map(_._1).max
-
   def matchLeft(left: Tile, right: Tile): Boolean = {
-    (0 to maxIdx).forall(y => left.pixels(maxIdx, y) == right.pixels(0, y))
+    (0 to left.maxX).forall(y => left.pixels(left.maxX, y) == right.pixels(0, y))
   }
 
   def matchNorth(up: Tile, down: Tile): Boolean = {
-    (0 to maxIdx).forall(x => up.pixels(x, maxIdx) == down.pixels(x, 0))
+    (0 to down.maxY).forall(x => up.pixels(x, down.maxY) == down.pixels(x, 0))
   }
 
   def neighs(leftNeigh: Option[Tile], northNeigh: Option[Tile], used: Set[Long]): Set[Tile] = {
     for {
       tile <- input
       if !used.contains(tile.id)
-      flipped <- Set(tile, tile.flipH, tile.flipV)
-      rotations <- 0 until 4
-      rotatedTile = flipped.rotateN(rotations)
-      if (leftNeigh.isEmpty || matchLeft(leftNeigh.get, rotatedTile))
-      if (northNeigh.isEmpty || matchNorth(northNeigh.get, rotatedTile))
+      rotatedTile <- tile.variations
+      if leftNeigh.isEmpty || matchLeft(leftNeigh.get, rotatedTile)
+      if northNeigh.isEmpty || matchNorth(northNeigh.get, rotatedTile)
     } yield rotatedTile
   }
 
   val rows = math.round(math.sqrt(input.size)).toInt
   val cols = rows
 
-  def seaMonster(tiles: Map[(Int, Int), Tile]): Unit = {
-    val shifted = for {
-      row <- 0 until rows
-      col <- 0 until cols
-      tile = tiles(row, col)
-      offsetX = (maxIdx - 1) * col - 1
-      offsetY = (maxIdx - 1) * row - 1
-      shiftedPixels = tile.pixels.filter {
-        case ((0, _), _) => false
-        case ((_, 0), _) => false
-        case ((`maxIdx`, _), _) => false
-        case ((_, `maxIdx`), _) => false
-        case _ => true
-      }.map {
-        case ((x, y), v) => (x + offsetX, y + offsetY) -> v
-      }
-    } yield shiftedPixels
-    val assembled = shifted.foldLeft(Map[(Int, Int), Boolean]()) {
-      case (acc, map) => map.foldLeft(acc) { case (z, (key, value)) =>
-        assert(!z.contains(key))
-        z.updated(key, value)
-      }
-    }
-    val tile = Tile(-1, assembled)
-    val seaMonster = Tile(-1, List(
-      "                  # ",
-      "#    ##    ##    ###",
-      " #  #  #  #  #  #   ",
-    ).zipWithIndex.flatMap {
-      case (row, y) => row.zipWithIndex.map {
-        case ('#', x) => (x, y) -> true
-        case (' ', x) => (x, y) -> false
-      }
-    }.toMap)
-    val monsters = for {
-      flipped <- Set(seaMonster, seaMonster.flipH, seaMonster.flipV)
-      rotations <- 0 until 4
-      rotatedMonster = flipped.rotateN(rotations)
-    } yield rotatedMonster
-    val monsterTiles = monsters.foldLeft(Set[(Int, Int)]()) {
-      case (acc, monster) => acc ++ overlap(monster, tile)
-    }
-    val roughness = tile.pixels.count {
-      case (x, true) if !(monsterTiles contains x) => true
-      case _ => false
-    }
-    println(s"rougness = $roughness")
+  def roughness(tiles: Map[(Int, Int), Tile]): Int = {
+    val tile = Tile(-1, assembleImage(tiles))
+    val hereBeDragons = for {
+      monster <- Tile.seaMonster.variations
+      monsterPixel <- overlappingPixels(monster, tile)
+    } yield monsterPixel
+    tile.pixels.count(px => !hereBeDragons.contains(px))
   }
 
-  def overlap(monster: Tile, tile: Tile): Set[(Int, Int)] = {
+  private def assembleImage(tiles: Map[(Int, Int), Tile]): Set[(Int, Int)] = {
+    val shifted = for {
+      row: Int <- (0 until rows).toSet
+      col <- 0 until cols
+      tile = tiles(row, col)
+      offsetX = (tile.maxX - 1) * col - 1
+      offsetY = (tile.maxY - 1) * row - 1
+      shiftedPixels = tile.pixels.filter {
+        case (0, _) => false
+        case (_, 0) => false
+        case (x, _) if x == tile.maxX => false
+        case (_, y) if y == tile.maxY => false
+        case _ => true
+      }.map {
+        case (x, y) => (x + offsetX, y + offsetY)
+      }
+    } yield shiftedPixels
+    shifted.flatten
+  }
+
+  def overlappingPixels(monster: Tile, tile: Tile): Set[(Int, Int)] = {
     (for {
       x <- 0 to tile.maxX - monster.maxX
       y <- 0 to tile.maxY - monster.maxY
-      shiftedMonster = monster.pixels.foldLeft(Set[(Int, Int)]()) {
-        case (acc, ((mx, my), true)) => acc ++ Set((x + mx, y + my))
-        case (acc, _) => acc
-      }
-      if (shiftedMonster.forall {
-        case (mx, my) => tile.pixels.getOrElse((mx, my), false)
-        case _ => false
-      })
+      shiftedMonster = monster.pixels.map { case (mx, my) => (x + mx, y + my) }
+      if shiftedMonster.forall(tile.pixels.contains)
       monsterPixel <- shiftedMonster
     } yield monsterPixel).toSet
   }
 
-  def solve(row: Int, col: Int, acc: Map[(Int, Int), Tile]): Set[Map[(Int, Int), Tile]] = {
-
-    val leftNeigh = if (col > 0) Some(acc(row, col - 1)) else None
-    val northNeigh = if (row > 0) Some(acc(row - 1, col)) else None
-    val ns = neighs(leftNeigh, northNeigh, acc.values.map(_.id).toSet)
-
-    if (acc.size == rows * cols) {
-      val sol = acc
-      seaMonster(sol)
-      Set(acc)
-    }
+  def solve(row: Int, col: Int, acc: Map[(Int, Int), Tile]): Option[Map[(Int, Int), Tile]] = {
+    if (acc.size == input.size) Some(acc)
     else {
+      val leftNeigh = if (col > 0) Some(acc(row, col - 1)) else None
+      val northNeigh = if (row > 0) Some(acc(row - 1, col)) else None
       for {
-        n <- ns
+        n <- neighs(leftNeigh, northNeigh, acc.values.map(_.id).toSet)
         sol <- solve(
           if (col == cols - 1) row + 1 else row,
           if (col == cols - 1) 0 else col + 1,
           acc.updated((row, col), n))
-      } yield sol
+      } return Some(sol)
+      None
     }
   }
 
-  val sol = solve(0, 0, Map()).head
+  val sol = solve(0, 0, Map()).get
 
   println(sol(0, 0).id * sol(0, cols - 1).id * sol(rows - 1, 0).id * sol(rows - 1, cols - 1).id)
+
+  println(roughness(sol))
 }
