15cc146d57dd8e41947981ce56d50ff8a04f0604
George Samidare <larzone@gmail.com>
2021-12-02 06:23:06 +0100

    y2021d02.cleanup

--- src/main/scala/aoc2021/Day02.scala
+++ src/main/scala/aoc2021/Day02.scala
@@ -5,28 +5,25 @@ import scala.util.Using
 
 object Day02 extends App {
 
-  val input = Using(Source.fromFile("inputs/2021/02.txt"))(_.getLines().toList).get
+  val input = Using(Source.fromFile("inputs/2021/02.txt"))(_.getLines()
+    .map(_.split(" ").toList)
+    .map {
+      case cmd :: amt :: Nil => (cmd, amt.toInt)
+    }.toList).get
 
-  println(input)
-
-  val cmds = for {
-    s <- input
-    cmd :: amt :: Nil = s.split(" ").toList
-  } yield (cmd, amt.toInt)
-
-  val pos = cmds.foldLeft((0, 0)) {
+  val p1 = input.foldLeft((0, 0)) {
     case ((h, d), ("forward", amt)) => (h + amt, d)
     case ((h, d), ("down", amt)) => (h, d + amt)
     case ((h, d), ("up", amt)) => (h, d - amt)
   }
 
-  println(pos._1 * pos._2)
+  println(p1._1 * p1._2)
 
-  val aim = cmds.foldLeft((0, 0, 0)) {
+  val p2 = input.foldLeft((0, 0, 0)) {
     case ((h, d, aim), ("forward", amt)) => (h + amt, d + aim * amt, aim)
     case ((h, d, aim), ("down", amt)) => (h, d, aim + amt)
     case ((h, d, aim), ("up", amt)) => (h, d, aim - amt)
   }
 
-  println(aim._1 * aim._2)
+  println(p2._1 * p2._2)
 }
