6419067ec2a0d4483cd6c21d50dc972b8ab221fc
George Samidare <larzone@gmail.com>
2021-12-11 14:09:31 +0100

    y2021d11.cleanup

--- src/main/scala/aoc2021/Day11.scala
+++ src/main/scala/aoc2021/Day11.scala
@@ -8,37 +8,32 @@ import scala.util.Using
 object Day11 extends App {
 
   val input = Using(Source.fromFile("inputs/2021/11.txt"))(_.getLines()
-    .map(s => s.map(ch => s"$ch".toInt).toList)
-    .toList).get
+    .map(s => s.map(ch => s"$ch".toInt).toVector)
+    .toVector).get
 
   val height = input.size
   val width = input.head.size
 
   def neighs(p: (Int, Int)): Set[(Int, Int)] = {
-    Set((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)).map {
-      case (j, i) => (p._1 + j, p._2 + i)
-    }.filter {
-      case (j, i) if j >= 0 && j < height && i >= 0 && i < width => true
-      case _ => false
-    }
+    for {
+      _ <- Set(())
+      dx <- -1 to 1
+      dy <- -1 to 1 if dy != 0 || dx != 0
+      x = p._2 + dx if x >= 0 && x < width
+      y = p._1 + dy if y >= 0 && y < height
+    } yield (y, x)
   }
 
-  def substep(octopi: List[List[Int]]): List[List[Int]] = {
-    val flashed = octopi.zipWithIndex.flatMap {
-      case (row, j) => row.zipWithIndex.flatMap {
-        case (10, i) => Some(j, i)
-        case _ => None
-      }
-    }.toSet
+  def substep(octopi: Vector[Vector[Int]]): Vector[Vector[Int]] = {
     octopi.zipWithIndex.map {
       case (row, j) => row.zipWithIndex.map {
-        case (oct, i) if flashed(j, i) || oct == 0 => 0
-        case (oct, i) => math.min(10, oct + neighs(j, i).intersect(flashed).size)
+        case (oct, _) if oct >= 10 || oct == 0 => 0
+        case (oct, i) => oct + neighs(j, i).count(p => octopi(p._1)(p._2) >= 10)
       }
     }
   }
 
-  def step(octopi: List[List[Int]]): List[List[Int]] = {
+  def step(octopi: Vector[Vector[Int]]): Vector[Vector[Int]] = {
     fixedPoint(substep)(octopi.map(_.map(_ + 1)))
   }
 
@@ -47,6 +42,6 @@ object Day11 extends App {
 
   println(
     LazyList.iterate(input)(step).zipWithIndex.dropWhile {
-      case (octopi, _) => octopi.flatten.toSet != Set(0)
+      case (octopi, _) => octopi.exists(_.exists(_ != 0))
     }.head._2)
 }
