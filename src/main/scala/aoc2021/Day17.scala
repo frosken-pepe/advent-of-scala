3ea42aa72779437ac37ff8cf50808a6d4c4e1a0a
George Samidare <larzone@gmail.com>
2021-12-17 06:31:51 +0100

    y2021d17

---
+++ src/main/scala/aoc2021/Day17.scala
@@ -0,0 +1,52 @@
+package aoc2021
+
+import scala.io.Source
+import scala.util.Using
+
+object Day17 extends App {
+
+  val input = Using(Source.fromFile("inputs/2021/17.txt"))(_.getLines()
+    .next()).get
+
+  val targetArea = input match {
+    case s"target area: x=$xmin..$xmax, y=$ymin..$ymax" => (xmin.toInt, xmax.toInt, ymin.toInt, ymax.toInt)
+  }
+
+  val (xmin, xmax, ymin, ymax) = targetArea
+
+  case class Probe(x: Int, y: Int, vx: Int, vy: Int)
+
+  def update(probe: Probe): Probe = {
+    val x = probe.x + probe.vx
+    val y = probe.y + probe.vy
+    val vx = if (probe.vx > 0) probe.vx - 1
+    else if (probe.vx == 0) 0
+    else probe.vx + 1
+    val vy = probe.vy - 1
+    Probe(x, y, vx, vy)
+  }
+
+  def shoot(vx: Int, vy: Int): Option[Int] = {
+    val ll = LazyList.unfold((("", Int.MinValue), Probe(0, 0, vx, vy))) {
+      case ((s, highest), p@Probe(x, y, _, _)) =>
+        val pp = math.max(highest, y)
+        if (x >= xmin && x <= xmax && y >= ymin && y <= ymax)
+          Some((s, pp), (("hit_target", pp), update(p)))
+        else if (y < ymin && s == "")
+          None
+        else
+          Some((s, pp), ((s, pp), update(p)))
+    }
+    val hd = ll.dropWhile(_._1 == "").headOption
+    hd.map(_._2)
+  }
+
+  val fired = for {
+    vx <- 1 until 500
+    vy <- -300 until 300
+    highest <- shoot(vx, vy)
+  } yield (highest, (vx, vy))
+
+  println(fired.map(_._1).max)
+  println(fired.length)
+}
