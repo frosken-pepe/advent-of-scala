beebe56994905197ee7b17ed00968603a1ef2aaa
George Samidare <larzone@gmail.com>
2022-12-11 10:20:35 +0100

    y2022d06

--- src/main/scala/aoc2022/Day06.scala
+++ src/main/scala/aoc2022/Day06.scala
@@ -1,6 +1,17 @@
 package aoc2022
 
+import scala.io.Source
+import scala.util.Using
+
 object Day06 extends App {
-  
-  
+
+  private val input: String = Using(Source.fromFile("inputs/2022/06.txt"))(_.getLines().next()).get
+
+  def marker(size: Int): Int = {
+    input.sliding(size, 1).toList.zipWithIndex.dropWhile(_._1.toSet.size != size).map(_._2).head + size
+  }
+
+  println(marker(4))
+
+  println(marker(14))
 }
