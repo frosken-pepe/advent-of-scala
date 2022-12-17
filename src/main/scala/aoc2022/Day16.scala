548c20045bd749f5f0a710986d8aa8f9ec4fce81
George Samidare <larzone@gmail.com>
2022-12-17 14:03:23 +0100

    y2022d16.p2.good_enough

--- src/main/scala/aoc2022/Day16.scala
+++ src/main/scala/aoc2022/Day16.scala
@@ -16,12 +16,7 @@ object Day16 extends App {
   val valves: Map[String, Valve] = Using(Source.fromFile("inputs/2022/16.txt"))(_.getLines().map(valve)
     .map(valve => valve.id -> valve).toMap).get
 
-  case class State(me: String, open: Set[String]) {
-    def applyAction(action: Action): State = action match {
-      case Open => copy(open = open + me)
-      case Move(target, _) => copy(me = target)
-    }
-  }
+  case class State(me: String, open: Set[String], unopened: Set[String], time: Int)
 
   sealed trait Action
 
@@ -29,7 +24,12 @@ object Day16 extends App {
 
   case class Move(target: String, time: Int) extends Action
 
-  val meaningfulValves = valves.values.filter(_.rate > 0).map(_.id).toSet
+  def action(state: State, action: Action): State = action match {
+    case Open => state.copy(open = state.open + state.me, unopened = state.unopened - state.me, time = state.time + 1)
+    case Move(target, dt) => state.copy(me = target, time = state.time + dt)
+  }
+
+  val nonZeroValves = valves.values.filter(_.rate > 0).map(_.id).toSet
 
   def expand(frontier: Set[String], visited: Set[String]): Set[String] = {
     frontier.flatMap(f => valves(f).neighs) -- visited
@@ -46,41 +46,42 @@ object Day16 extends App {
   }
 
   val shortestPaths = (for {
-    a <- meaningfulValves + "AA"
-    b <- meaningfulValves
+    a <- nonZeroValves + "AA"
+    b <- nonZeroValves
     if a < b
   } yield (a, b, shortestPath(a, b))).flatMap {
     case (a, b, d) => List((a, b) -> d, (b, a) -> d)
   }.toMap
 
-  def actions(state: State, time: Int, endTime: Int, meaningfulValves: Set[String]): Set[Action] = {
-    if (state.open == meaningfulValves) Set()
+  def actions(state: State, endTime: Int): Set[Action] = {
+    if (state.unopened.isEmpty) Set()
     else if (!state.open(state.me) && valves(state.me).rate > 0) Set(Open)
-    else meaningfulValves
-      .filter(_ != state.me)
-      .filter(!state.open(_))
+    else state.unopened
       .map(valve => Move(valve, shortestPaths(valve, state.me)))
-      .filter(_.time + time <= endTime)
+      .filter(_.time + state.time <= endTime)
       .toSet
   }
 
-  def backtrack(time: Int, current: State, pressure: Int, visited: Set[State], endTime: Int, meaningfulValves: Set[String]): Int = {
-    if (time == endTime) pressure
-    else actions(current, time, endTime, meaningfulValves).map { a => (a, current.applyAction(a)) }
-      .filter { case (_, state) => !visited(state) }
-      .map {
-        case (Open, s) => backtrack(time + 1, s, pressure + valves(current.me).rate * (endTime - time - 1), visited + s, endTime, meaningfulValves)
-        case (Move(_, deltaT), s) => backtrack(time + deltaT, s, pressure, visited + s, endTime, meaningfulValves)
-      }
-      .foldLeft(pressure)(math.max)
+  def backtrack(state: State, pressure: Int, endTime: Int): Int = {
+    if (state.time == endTime) pressure
+    else actions(state, endTime).map {
+      case Open => backtrack(action(state, Open), pressure + valves(state.me).rate * (endTime - state.time - 1), endTime)
+      case Move(to, dt) => backtrack(action(state, Move(to, dt)), pressure, endTime)
+    }.foldLeft(pressure)(math.max)
   }
 
-  println(backtrack(0, State("AA", Set()), 0, Set(), 30, meaningfulValves))
+  println(backtrack(State("AA", Set(), nonZeroValves, 0), 0, 30))
 
-  println(
-    (for {
-      me <- meaningfulValves.subsets()
-      elephant = meaningfulValves -- me
-    } yield backtrack(0, State("AA", Set()), 0, Set(), 26, me) +
-      backtrack(0, State("AA", Set()), 0, Set(), 26, elephant)).max)
+  def justTheTwoOfUs(subsets: Set[Set[String]]): Int = {
+    subsets.map(State("AA", Set(), _, 0)).map(backtrack(_, 0, 26)).sum
+  }
+
+  val partitions: Set[Set[Set[String]]] = (for {
+    me <- nonZeroValves.subsets()
+    elephant = nonZeroValves -- me
+    if (me.size - elephant.size).abs < 2 // assume approx. equal assignment of valves
+    u = Set(me, elephant)
+  } yield u).toSet
+
+  println(partitions.map(justTheTwoOfUs).max)
 }
